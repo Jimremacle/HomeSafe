97cf30641d5ae7da4e839c71fdbfa8a2bbef1bd9
Krishna Krishna <kvanst3@gmail.com>
2019-06-04 13:05:56 +0200

    setting up pundit and report controller

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -1,2 +1,37 @@
 class ReportsController < ApplicationController
+  before_action :set_reports, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @reports = policy_scope(Report)
+  end
+
+  def show
+  end
+
+  def new
+    @report = Report.new
+    authorize @report
+  end
+
+  def edit
+  end
+
+  def create
+    @report = report.new(report_params)
+    @report.user = current_user
+    authorize @report
+
+    if @report.save
+      redirect_to @report, notice: 'Report created successfully!'
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def set_reports
+    @report = Report.find(params[:id])
+    authorize @report
+  end
 end
