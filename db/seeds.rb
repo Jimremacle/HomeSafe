701e6ee6ef45ff22d27708a05ae3db5496b59746
Krishna Krishna <kvanst3@gmail.com>
2019-06-04 13:57:23 +0200

    seed file clean

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,107 +1,111 @@
 require 'faker'
 
+Report.delete_all
+User.delete_all
+
 5.times do
   ename = Faker::Internet.email
   epass = Faker::Internet.password
   address = Faker::Address.street_address
   photo = Faker::Avatar.image("my-own-slug", "50x50")
-  User.create(email: ename, password: epass, frequent_address: address, avatar: photo)
+  usercreated = User.create(email: ename, password: epass, frequent_address: address, avatar: photo)
   puts "Done Seeding..."
+  @userId = usercreated.id
 end
-
-Report.delete_all
 
 reports_attribute = [
   {
-    Type: "Verbal",
+    report_type: "Verbal",
     description:"A man insulted me",
     longitude:4.3641,
     latitude:50.8454,
     occurence_timedate: "2019/06/05 20:50:00",
     address: "Cantersteen 10 Bruxelles",
-    user_id: 1
+    user_id: (@userId - 4)
   },
   {
-    Type: "Physical",
+    report_type: "Physical",
     description:"A man tried to take my hand",
     longitude:4.3641,
     latitude:50.8464,
     occurence_timedate: "2019-06-05 21:50:00",
     address: "Place de la Nation Bruxelles",
-    user_id: 1
+    user_id: (@userId - 4)
   },
   {
-    Type: "Physical",
+    report_type: "Physical",
     description:"A man chased me",
     longitude:4.4338,
     latitude:50.8167,
     occurence_timedate: "2019-06-06 21:50:00",
     address: "Avenue Roger Hainault Auderghem",
-    user_id: 2
+    user_id: (@userId - 3)
   },
   {
-    Type: "Verbal",
+    report_type: "Verbal",
     description:"A man yelled at me",
     longitude:4.4038,
     latitude:50.8667,
     occurence_timedate: "2019-06-06 21:50:00",
     address: "Avenue des Loisirs 11 Evere",
-    user_id: 2
+    user_id: (@userId - 3)
   },
   {
-    Type: "Physical",
+    report_type: "Physical",
     description:"A man chased me",
     longitude:4.350585,
     latitude:50.844805,
     occurence_timedate: "2019-06-06 21:50:00",
     address: "Stalingrad Bruxelles",
-    user_id: 3
+    user_id: (@userId - 2)
   },
   {
-    Type: "Feeling",
+    report_type: "Feeling",
     description:"Creepy neighborhood",
     longitude:4.344922,
     latitude:50.840470,
     occurence_timedate: "2019-06-06 21:50:00",
     address: "Marolles Bruxelles",
-    user_id: 3
+    user_id: (@userId - 2)
   },
   {
-    Type: "Feeling",
+    report_type: "Feeling",
     description:"Bad neighborhood",
     longitude:4.344792,
     latitude:50.847377,
     occurence_timedate: "2019-06-06 21:50:00",
     address: "Rue Van Artevelde 55 Bruxelles",
-    user_id: 4
+    user_id: (@userId - 1)
   },
   {
-    Type: "Verbal",
+    report_type: "Verbal",
     description:"A man wanted to sell me drugs",
     longitude:4.352064,
     latitude:50.852751,
     occurence_timedate: "2019-06-06 21:50:00",
     address: "Rue Vander Elst 17 Bruxelles",
-    user_id: 4
+    user_id: (@userId - 1)
   },
   {
-    Type: "Physicak",
+    report_type: "Physicak",
     description:"Saw a huge fight",
     longitude:4.369154,
     latitude:50.858768,
     occurence_timedate: "2019-06-06 21:50:00",
     address: "Place de la Reine Bruxelles",
-    user_id: 5
+    user_id: @userId
   },
   {
-    Type: "Feeling",
+    report_type: "Feeling",
     description:"A man wanted to sell me drugs",
     longitude:4.363870,
     latitude:50.860309,
     occurence_timedate: "2019-06-06 21:50:00",
     address: "Rue de la Fraternité Schaerbeek",
-    user_id: 5
+    user_id: @userId
   }
 ]
+
+Report.create!(reports_attribute)
 puts "Done Seeding..."
 
